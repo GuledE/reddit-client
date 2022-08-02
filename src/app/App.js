c90caac5a4edea79e3da2b39fe3642e0cd0b6220
GuledE <99482335+GuledE@users.noreply.github.com>
2022-08-02 16:40:28 +0100

    setting up remote

---
+++ src/app/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <>
+    <div className='nav'>
+      <h1 className='logo'>Title/NAV</h1>
+     </div>
+    <div className='main'>
+     <p>CardList</p>
+     <p>CardList</p>
+    </div>
+    <footer className='footer'>
+      <p className='footer-text'>NNNNNNNNNNNNNNNNNNNNNNNNNNN</p>
+    </footer>
+    </>
+  )
+}
+
+export default App;
