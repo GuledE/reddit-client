debe6fb390811c5783aa4a97b421026a384e2cb7
GuledE <99482335+GuledE@users.noreply.github.com>
2022-08-07 09:42:46 +0100

    re-did the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,12 @@ import React from 'react'
   export default function Cards(props) {
     const { post } = props;
     return (
-      
+        <div>
+      <h1>{post.userId} {post.title}</h1>
+      <section>
+        {post.body}
+      </section>
+      {post.id}
+      </div>
     );
   }
